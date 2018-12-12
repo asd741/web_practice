f29d92d0b3cec400e8091a8dfa4ef3a9dbb66c51
asd741 <a22833593@gmail.com>
2018-12-12 23:03:08 +0800

    處理linten

--- chatroom/server.js
+++ chatroom/server.js
@@ -1,11 +1,14 @@
 var http=require('http');
 var fs=require('fs');
 var ws=require('socket.io');
+const port = process.env.PORT || 3000;
 var server=http.createServer(function (req,res) {
     var html=fs.readFileSync('./index.htm');
     res.end(html);
 });
-server.listen("","https://asd741.nctu.me/chatroom/");
+server.listen(port, () => {
+    console.log("Server Started. http://localhost:" + port);
+});
 var io=ws(server);
 io.on("connection",function(socket) {
     socket.on("message",function(msg) {
